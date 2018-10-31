d5a78a5c75513bf8f30cf2b44a70551e50f82ae1
James Sheehan <jmsdevx@gmail.com>
2018-10-31 15:24:45 -0500

    added grammar api functionality

--- src/components/homework/Results.js
+++ src/components/homework/Results.js
@@ -5,28 +5,90 @@ require("dotenv").config();
 class Results extends Component {
   constructor() {
     super();
-    this.state = { homework: [] };
+    this.state = {
+      homework: [],
+      content: "",
+      errors: { errors: [{ bad: "click get to see results" }, { type: "" }] },
+      check: false
+    };
     this.getHomework = this.getHomework.bind(this);
+    this.drill = this.drill.bind(this);
+    this.getResults = this.getResults.bind(this);
   }
 
   componentDidMount() {
     this.getHomework();
   }
 
-  getHomework() {
-    axios
+  async getHomework() {
+    await axios
       .get(`http://localhost:3005/api/homework/${this.props.match.params.id}`)
-      .then(response => this.setState({ homework: response.data }));
+      .then(response =>
+        this.setState({ homework: response.data, check: false })
+      );
+    this.drill();
   }
 
   getResults() {
-    axios.post();
+    axios
+      .post(
+        `https://api.textgears.com/check.php?text=${
+          this.state.content
+        }?&key=HzehDNgOrWUNFEOk`
+      )
+      .then(response => this.setState({ errors: response.data }));
+  }
+
+  drill() {
+    let alltext = [];
+    this.state.homework[0].hw_content.blocks.map((e, i) =>
+      alltext.push(e.text)
+    );
+    let newtext = alltext.join(" ");
+    this.setState({ content: newtext, check: !this.state.check });
+    console.log(this.state.content);
   }
 
   render() {
+    let errordisplay = "";
+    let typedisplay = "";
+    if (this.state.check) {
+      errordisplay = this.state.errors.errors.map((e, i) => {
+        return (
+          <div key={i}>
+            <h3>{e.bad}</h3>
+          </div>
+        );
+      });
+      typedisplay = this.state.errors.errors.map((e, i) => {
+        return (
+          <div key={i}>
+            <h3>{e.type}</h3>
+          </div>
+        );
+      });
+    }
+
     return (
-      <div>
+      <div className="resultsContainer">
         <h1>Results</h1>
+        <button onClick={this.getResults}>Get</button>
+        <div className="hwErrorBox">
+          <div className="yourHomework">
+            <h2>Your Homework: </h2>
+            <h3>{this.state.content}</h3>
+          </div>
+          <div className="errors">
+            <div>
+              <h2>Errors:</h2>
+              {errordisplay}
+            </div>
+            <div>
+              <h2>Type:</h2>
+              {typedisplay}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="container">
-        <Link to="/">My Dashboard</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/homework/all">My Homework</Link>
-        <Link to="/homework/write">Write</Link>
-      </div>
+      <Link to="/">My Dashboard</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/homework/all">My Homework</Link>
+      <Link to="/homework/write">Write</Link>
     </nav>
   );
 };
