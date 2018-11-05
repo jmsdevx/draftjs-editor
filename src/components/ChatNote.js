38338419651ecff80564c4d25c7789046b3b4e84
James Sheehan <jmsdevx@gmail.com>
2018-11-05 00:14:59 -0600

    testing notepad with video chat

---
+++ src/components/ChatNote.js
@@ -0,0 +1,196 @@
+import React, { Component } from "react";
+import { EditorState, RichUtils, convertToRaw } from "draft-js";
+import Editor from "draft-js-plugins-editor";
+import createHighlightPlugin from "./plugins/highlightPlugin";
+import addLinkPlugin from "./plugins/addLinkPlugin";
+import BlockStyleToolbar, {
+  getBlockStyle
+} from "./blockstyles/BlockStyleToolbar";
+import axios from "axios";
+// import { connect } from "react-redux";
+
+const highlightPlugin = createHighlightPlugin();
+
+class PageContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      displayedNote: "new",
+      hw_title: "My Homework"
+    };
+    this.onChange = editorState => this.setState({ editorState });
+    this.plugins = [highlightPlugin, addLinkPlugin];
+  }
+
+  // componentDidMount() {
+  //   if (this.props.note === null) {
+  //     this.setState({
+  //       displayedNote: "new",
+  //       editorState: EditorState.createEmpty()
+  //     });
+  //   } else {
+  //     this.setState({
+  //       displayedNote: this.props.note.id,
+  //       editorState: EditorState.createWithContent(
+  //         convertFromRaw(JSON.parse(this.props.note.content))
+  //       )
+  //     });
+  //   }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.note == null && !!this.props.note) {
+  //     this.props.loadNote();
+  //     this.setState({
+  //       displayedNote: this.props.note.id,
+  //       editorState: EditorState.createWithContent(
+  //         convertFromRaw(JSON.parse(this.props.note.content))
+  //       )
+  //     });
+  //   }
+  // }
+
+  // submitEditor = () => {
+  //   let contentState = this.state.editorState.getCurrentContent();
+  //   if (this.state.displayedNote === "new") {
+  //     let note = { content: convertToRaw(contentState) };
+  //     note["content"] = JSON.stringify(note.content);
+  //     this.props.createNote(note.content);
+  //   } else {
+  //     let note = { content: convertToRaw(contentState) };
+  //     note["content"] = JSON.stringify(note.content);
+  //     this.props.updateNote(this.state.displayedNote, note.content);
+  //   }
+  // };
+
+  submitEditor = () => {
+    let contentState = this.state.editorState.getCurrentContent();
+    let note = { content: convertToRaw(contentState) };
+    console.log(note);
+    let hw_content = JSON.stringify(note.content);
+    console.log("title: " + this.state.hw_title);
+    console.log(hw_content);
+    const { student_id, hw_title } = this.state;
+    axios
+      .post("http://localhost:3005/api/homework", {
+        student_id: student_id,
+        hw_title: hw_title,
+        hw_content: hw_content
+      })
+      .then();
+  };
+
+  handleKeyCommand = command => {
+    const newState = RichUtils.handleKeyCommand(
+      this.state.editorState,
+      command
+    );
+    if (newState) {
+      this.onChange(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
+
+  onUnderlineClick = () => {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+    );
+  };
+
+  onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+  };
+
+  onItalicClick = () => {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+    );
+  };
+
+  onHighlight = () => {
+    this.onChange(
+      RichUtils.toggleInlineStyle(this.state.editorState, "HIGHLIGHT")
+    );
+  };
+
+  onAddLink = () => {
+    const editorState = this.state.editorState;
+    const selection = editorState.getSelection();
+    const link = window.prompt("Paste the link -");
+    if (!link) {
+      this.onChange(RichUtils.toggleLink(editorState, selection, null));
+      return "handled";
+    }
+    const content = editorState.getCurrentContent();
+    const contentWithEntity = content.createEntity("LINK", "MUTABLE", {
+      url: link
+    });
+    const newEditorState = EditorState.push(
+      editorState,
+      contentWithEntity,
+      "create-entity"
+    );
+    const entityKey = contentWithEntity.getLastCreatedEntityKey();
+    this.onChange(RichUtils.toggleLink(newEditorState, selection, entityKey));
+  };
+
+  toggleBlockType = blockType => {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
+  };
+
+  titleChange = input => {
+    this.setState({ hw_title: input });
+    console.log(this.state.hw_title);
+  };
+
+  render() {
+    return (
+      <div className="editorContainer">
+        <input
+          type="text"
+          value={this.state.hw_title}
+          onChange={e => this.titleChange(e.target.value)}
+        />
+        <div className="btncontainer">
+          <button onClick={this.onUnderlineClick}>U</button>
+          <button onClick={this.onBoldClick}>
+            <b>B</b>
+          </button>
+          <button onClick={this.onItalicClick}>
+            <em>I</em>
+          </button>
+          <button onClick={this.onHighlight}>
+            <span style={{ background: "yellow" }}>H</span>
+          </button>
+          <button onClick={this.onAddLink}>LINK</button>
+        </div>
+        <div className="myeditor">
+          <BlockStyleToolbar
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange}
+            onToggle={this.toggleBlockType}
+          />
+          <Editor
+            blockStyleFn={getBlockStyle}
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            handleKeyCommand={this.handleKeyCOmmand}
+            plugins={this.plugins}
+          />
+        </div>
+        <button onClick={this.submitEditor}>Submit</button>
+      </div>
+    );
+  }
+}
+
+// function mapStatetoProps(state) {
+//   return { state };
+// }
+
+// export default connect(mapStatetoProps)(PageContainer);
+
+export default PageContainer;
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
 import { Card, CardText } from "material-ui/Card";
+import PageContainer from "../PageContainer1";
 
 export default class Chat extends Component {
   constructor(props) {
@@ -15,11 +16,23 @@ export default class Chat extends Component {
       previewTracks: null,
       localMediaAvailable: false,
       hasJoinedRoom: false,
-      activeRoom: "" // Track the current active room
+      activeRoom: null // Track the current active room
     };
     this.joinRoom = this.joinRoom.bind(this);
     this.handleRoomNameChange = this.handleRoomNameChange.bind(this);
     this.roomJoined = this.roomJoined.bind(this);
+    this.leaveRoom = this.leaveRoom.bind(this);
+    this.attachTracks = this.attachTracks.bind(this);
+    this.attachParticipantTracks = this.attachParticipantTracks.bind(this);
+    this.detachTracks = this.detachTracks.bind(this);
+    this.detachParticipantTracks = this.detachParticipantTracks.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get("/token").then(results => {
+      const { identity, token } = results.data;
+      this.setState({ identity, token });
+    });
   }
 
   handleRoomNameChange(e) {
@@ -50,6 +63,11 @@ export default class Chat extends Component {
     ).then(this.roomJoined, error => {
       alert("Could not connect to Twilio: " + error.message);
     });
+  }
+
+  leaveRoom() {
+    this.state.activeRoom.disconnect();
+    this.setState({ hasJoinedRoom: false, localMediaAvailable: false });
   }
 
   attachTracks(tracks, container) {
@@ -87,16 +105,57 @@ export default class Chat extends Component {
     });
 
     // Attach LocalParticipant's Tracks, if not already attached.
-    var previewContainer = this.refs.localMedia;
+    let previewContainer = this.refs.localMedia;
     if (!previewContainer.querySelector("video")) {
       this.attachParticipantTracks(room.localParticipant, previewContainer);
     }
-  }
 
-  componentDidMount() {
-    axios.get("/token").then(results => {
-      const { identity, token } = results.data;
-      this.setState({ identity, token });
+    room.participants.forEach(participant => {
+      console.log("Already in Room: '" + participant.identity + "'");
+      var previewContainer = this.refs.remoteMedia;
+      this.attachParticipantTracks(participant, previewContainer);
+    });
+
+    // Participant joining room
+    room.on("participantConnected", participant => {
+      console.log("Joining: '" + participant.identity + "'");
+    });
+
+    // Attach participant’s tracks to DOM when they add a track
+    room.on("trackAdded", (track, participant) => {
+      console.log(participant.identity + " added track: " + track.kind);
+      var previewContainer = this.refs.remoteMedia;
+      this.attachTracks([track], previewContainer);
+    });
+
+    // Detach participant’s track from DOM when they remove a track.
+    room.on("trackRemoved", (track, participant) => {
+      console.log(participant.identity + " removed track: " + track.kind);
+      this.detachTracks([track]);
+    });
+
+    // Detach all participant’s track when they leave a room.
+    room.on("participantDisconnected", participant => {
+      console.log("Participant '" + participant.identity + "' left the room");
+      this.detachParticipantTracks(participant);
+    });
+
+    // Once the local participant leaves the room, detach the Tracks
+    // of all other participants, including that of the LocalParticipant.
+    room.on("disconnected", () => {
+      if (this.state.previewTracks) {
+        this.state.previewTracks.forEach(track => {
+          track.stop();
+        });
+      }
+      this.detachParticipantTracks(room.localParticipant);
+      room.participants.forEach(this.detachParticipantTracks);
+      // this.state.activeRoom = null
+      this.setState({
+        activeRoom: null,
+        hasJoinedRoom: false,
+        localMediaAvailable: false
+      });
     });
   }
 
@@ -114,31 +173,35 @@ export default class Chat extends Component {
       <RaisedButton
         label="Leave Room"
         secondary={true}
-        onClick={() => alert("Leave Room")}
+        onClick={this.leaveRoom}
       />
     ) : (
       <RaisedButton label="Join Room" primary={true} onClick={this.joinRoom} />
     );
     return (
-      <Card>
-        <CardText>
-          <div className="flex-container">
-            {showLocalTrack}
-            <div className="flex-item">
-              <TextField
-                hintText="Room Name"
-                onChange={this.handleRoomNameChange}
-                errorText={
-                  this.state.roomNameErr ? "Room Name is required" : undefined
-                }
-              />
-              <br />
-              {joinOrLeaveRoomButton}
+      <div>
+        <Card>
+          <CardText>
+            <div className="flex-container">
+              {showLocalTrack}
+              <div className="flex-item">
+                <TextField
+                  hintText="Room Name"
+                  onChange={this.handleRoomNameChange}
+                  errorText={
+                    this.state.roomNameErr ? "Room Name is required" : undefined
+                  }
+                />
+                <br />
+                {joinOrLeaveRoomButton}
+              </div>
+              {/*possible bug area*/}
+              <div className="flex-item" ref="remoteMedia" id="remote-media" />
             </div>
-            <div className="flex-item" ref="remoteMedia" id="remote-media" />
-          </div>
-        </CardText>
-      </Card>
+          </CardText>
+        </Card>
+        <PageContainer />
+      </div>
     );
   }
 }
